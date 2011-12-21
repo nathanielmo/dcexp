64c7872591b322a92bc852969cd61a3e67e5499e
nathanielmo <nathanielmo@datacom.co.nz>
2011-12-21 01:13:08 +0000

    blah

---
+++ app.js
@@ -0,0 +1,10 @@
+//var port = (process.env.VMC_APP_PORT || 3000);
+//var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VMC_APP_PORT || process.env.PORT);
+var host = (process.env.VCAP_APP_HOST || '0.0.0.0');
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Worrld\n');
+}).listen(port, host);
